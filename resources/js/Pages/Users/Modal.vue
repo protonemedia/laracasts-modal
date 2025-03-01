95c4675d5d60db04dd0c261e8acf39eb72e4baf3
Pascal Baljet <pascal@protone.media>
2025-03-01 13:37:03 +0100

    Episode 3

--- resources/js/Pages/Users/Modal.vue
+++ resources/js/Pages/Users/Modal.vue
@@ -9,14 +9,41 @@ defineProps({
 
 <template>
     <teleport to="body">
-        <div
-            v-show="show"
-            class="fixed inset-0 flex size-full items-center justify-center"
-        >
-            <div class="fixed inset-0 z-40 size-full bg-black/75" />
-            <div class="z-50 w-full max-w-md rounded-lg bg-white p-4">
-                <slot />
+        <transition leave-active-class="duration-300">
+            <div
+                v-show="show"
+                class="fixed inset-0 flex size-full items-center justify-center"
+            >
+                <transition
+                    enter-from-class="opacity-0"
+                    enter-to-class="opacity-100"
+                    enter-active-class="transition duration-300"
+                    leave-from-class="opacity-100"
+                    leave-to-class="opacity-0"
+                    leave-active-class="transition duration-300"
+                >
+                    <div
+                        v-show="show"
+                        class="fixed inset-0 z-40 size-full bg-black/75"
+                    />
+                </transition>
+
+                <transition
+                    enter-from-class="opacity-0 scale-90"
+                    enter-to-class="opacity-100 scale-100"
+                    enter-active-class="transition duration-300"
+                    leave-from-class="opacity-100 scale-100"
+                    leave-to-class="opacity-0 scale-90"
+                    leave-active-class="transition duration-300"
+                >
+                    <div
+                        v-show="show"
+                        class="z-50 w-full max-w-md rounded-lg bg-white p-4"
+                    >
+                        <slot />
+                    </div>
+                </transition>
             </div>
-        </div>
+        </transition>
     </teleport>
 </template>
